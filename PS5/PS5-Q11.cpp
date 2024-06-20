47b76a7811de73cf4feb45e6ad086186400e2e6b
sury666 <154620691+sury666@users.noreply.github.com>
2024-06-21 01:42:58 +0530

    Create PS5-Q11.cpp

---
+++ PS5/PS5-Q11.cpp
@@ -0,0 +1,2 @@
+//If there are any adjacent wolf and sheep, then the answer is No
+//Otherwise it is Yes and one of the solution says to put the required number of dogs ssurrounding each sheep to protect it from all sides.
